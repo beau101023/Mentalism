9104618e8cf44bdf8a5aa344ae29f36db3fd126a
Beau Baer <bbaer10000@gmail.com>
2023-03-13 01:27:27 -0600

    Minor cleanup

--- src/main/java/me/beaubaer/mentalism/gui/screens/MeditationScreen.java
+++ src/main/java/me/beaubaer/mentalism/gui/screens/MeditationScreen.java
@@ -2,7 +2,6 @@ package me.beaubaer.mentalism.gui.screens;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.FastColor;
