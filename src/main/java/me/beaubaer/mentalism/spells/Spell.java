ab659b5f4e7eb3977f6edf9682021adde0db4403
Beau Baer <bbaer10000@gmail.com>
2023-03-19 16:32:38 -0600

    started implementation of spells

--- src/main/java/me/beaubaer/mentalism/spells/Spell.java
+++ src/main/java/me/beaubaer/mentalism/spells/Spell.java
@@ -5,7 +5,7 @@ import net.minecraftforge.registries.ForgeRegistryEntry;
 /**
  * representation of a context-sensitive effect triggered via the radial menu
  */
-public class Spell extends ForgeRegistryEntry<Spell>
+public abstract class Spell extends ForgeRegistryEntry<Spell>
 {
-
+    public abstract void activate();
 }
