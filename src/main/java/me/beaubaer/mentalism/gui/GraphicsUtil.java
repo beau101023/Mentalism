a2e671fde5bbc76decc068b15de7ae26a10a9359
Beau Baer <bbaer10000@gmail.com>
2023-03-19 16:30:41 -0600

    added more utility methods

--- src/main/java/me/beaubaer/mentalism/gui/GraphicsUtil.java
+++ src/main/java/me/beaubaer/mentalism/gui/GraphicsUtil.java
@@ -5,11 +5,38 @@ import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Matrix4f;
 import com.mojang.math.Quaternion;
 import com.mojang.math.Vector3f;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FastColor;
 
 public class GraphicsUtil
 {
+    public static void renderRadialSeparators(Matrix4f pose, int segAmount, float innerRadius, float outerRadius, float centerX, float centerY, int color)
+    {
+        for(float i = 0; i<(2*Math.PI); i+= (2*Math.PI)/segAmount)
+        {
+            float length = outerRadius-innerRadius;
+
+            Matrix4f linePose = pose.copy();
+
+            // translate line to the center
+            linePose.translate(new Vector3f(centerX, centerY, 0));
+
+            // rotate line
+            Quaternion lineRot = Quaternion.fromXYZ(0, 0, i);
+            linePose.multiply(lineRot);
+
+            // offset line from the center
+            Matrix4f translator = Matrix4f.createTranslateMatrix(innerRadius, 0, 0);
+            linePose.multiply(translator);
+
+            line(linePose, 1, 0, 0, 0f, length, color);
+
+        }
+    }
+
     public static void line(Matrix4f pose, float lineWidth, int x1, int y1, int x2, int y2, int color)
     {
         Vector3f pos1 = new Vector3f(x1, y1, 0);
@@ -19,23 +46,35 @@ public class GraphicsUtil
         Vector3f pointingVector = pos2.copy();
         pointingVector.sub(pos1);
 
-        float rotation = angleFromX(pointingVector);
-        float length = magnitude(pointingVector);
+        float rotation = angleFromXHat2D(pointingVector.x(), pointingVector.y());
+        float length = magnitude2D(pointingVector.x(), pointingVector.y());
 
         line(pose, lineWidth, x1, y1, rotation, length, color);
     }
 
     public static void line(Matrix4f pose, float lineWidth, int x1, int y1, float rotation, float length, int color)
     {
-        Quaternion rotQuat = Quaternion.fromXYZ(new Vector3f(0, 0, rotation));
         Matrix4f innerPose = pose.copy();
         innerPose.translate(new Vector3f(x1, y1, 0));
+        Quaternion rotQuat = Quaternion.fromXYZ(new Vector3f(0, 0, rotation));
         innerPose.multiply(rotQuat);
 
         innerFill(innerPose, 0, -lineWidth/2, length, lineWidth/2, color);
     }
 
-    public static void renderTextureOverlay(Matrix4f pMatrix, float pMinX, float pMinY, float pMaxX, float pMaxY, ResourceLocation pTextureLocation, float pAlpha) {
+    public static void debugString(PoseStack ps, String str, int offset, int width, int height)
+    {
+        int textColor = FastColor.ARGB32.color(255, 255, 255, 255);
+        Gui.drawCenteredString(ps, Minecraft.getInstance().font, str, width/2, (height/2)+offset, textColor);
+    }
+
+    public static void renderSquareTextureOverlay(Matrix4f pMatrix, float radius, float centerX, float centerY, ResourceLocation pTextureLocation, float pAlpha)
+    {
+        renderTextureOverlay(pMatrix, centerX-radius, centerY-radius, centerX+radius, centerY+radius, pTextureLocation, pAlpha);
+    }
+
+    public static void renderTextureOverlay(Matrix4f pMatrix, float pMinX, float pMinY, float pMaxX, float pMaxY, ResourceLocation pTextureLocation, float pAlpha)
+    {
         RenderSystem.disableDepthTest();
         RenderSystem.depthMask(false);
         RenderSystem.enableBlend();
