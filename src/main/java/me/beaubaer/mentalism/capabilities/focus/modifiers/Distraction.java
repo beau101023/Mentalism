32090f9c1d38c7485dab66da5272b5517941cbc3
Beau Baer <bbaer10000@gmail.com>
2023-03-30 17:28:21 -0600

    Added id for sound-based distractions and spellcasting based distractions

--- src/main/java/me/beaubaer/mentalism/capabilities/focus/modifiers/Distraction.java
+++ src/main/java/me/beaubaer/mentalism/capabilities/focus/modifiers/Distraction.java
@@ -5,6 +5,11 @@ import me.beaubaer.mentalism.capabilities.focus.Focus;
 
 public class Distraction extends DecayingFocusModifier
 {
+    public static final String SPELL_DISTRACTION = "mentalism.distraction.spell";
+    public static final int SPELL_DISTRACTION_PRIORITY = 3;
+
+    public static final String SOUND_DISTRACTION = "mentalism.distraction.sound";
+
     public Distraction(Focus parent, int priority, float amount, float decayTime, String ID)
     {
         super(parent, priority, amount, decayTime, ID);
