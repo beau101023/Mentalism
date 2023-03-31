c78198bd01725a1848d64c8e0bb8edd7f0d680b5
Beau Baer <bbaer10000@gmail.com>
2023-03-31 09:57:05 -0600

    Made all sounds distract the player Added UnlockState and SpellManager capabilities to player Implemented basic unlockstate logic updated todo.txt

--- src/main/java/me/beaubaer/mentalism/events/ServerEvents.java
+++ src/main/java/me/beaubaer/mentalism/events/ServerEvents.java
@@ -1,32 +1,35 @@
 package me.beaubaer.mentalism.events;
 
 import me.beaubaer.mentalism.Mentalism;
-import me.beaubaer.mentalism.capabilities.focus.Focus;
 import me.beaubaer.mentalism.capabilities.focus.FocusProvider;
+import me.beaubaer.mentalism.capabilities.focus.IFocus;
 import me.beaubaer.mentalism.capabilities.focus.modifiers.AntiDistraction;
-import me.beaubaer.mentalism.networking.FocusValueSyncS2CPacket;
-import me.beaubaer.mentalism.networking.MentalismMessages;
-import net.minecraft.Util;
+import me.beaubaer.mentalism.capabilities.focus.modifiers.Distraction;
+import me.beaubaer.mentalism.capabilities.spellmanager.SpellManager;
+import me.beaubaer.mentalism.capabilities.spellmanager.SpellManagerProvider;
+import me.beaubaer.mentalism.capabilities.unlocks.UnlockState;
+import me.beaubaer.mentalism.capabilities.unlocks.UnlockStateProvider;
+import me.beaubaer.mentalism.registries.SpellRegistry;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.VanillaGameEvent;
+import net.minecraftforge.event.entity.PlaySoundAtEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 @Mod.EventBusSubscriber(modid = Mentalism.MOD_ID)
 public class ServerEvents
 {
+
     @SubscribeEvent
     public static void attachCapabilities(AttachCapabilitiesEvent<Entity> e)
     {
@@ -34,7 +37,15 @@ public class ServerEvents
         {
             if(!p.getCapability(FocusProvider.FOCUS).isPresent())
             {
-                e.addCapability(new ResourceLocation(Mentalism.MOD_ID, "properties"), new FocusProvider());
+                e.addCapability(new ResourceLocation(Mentalism.MOD_ID, "focus"), new FocusProvider());
+            }
+            if(!p.getCapability(SpellManagerProvider.SPELL_MANAGER).isPresent())
+            {
+                e.addCapability(new ResourceLocation(Mentalism.MOD_ID, "spellmanager"), new SpellManagerProvider());
+            }
+            if(!p.getCapability(UnlockStateProvider.UNLOCK_STATE).isPresent())
+            {
+                e.addCapability(new ResourceLocation(Mentalism.MOD_ID, "unlockmanager"), new UnlockStateProvider());
             }
         }
     }
@@ -53,7 +64,9 @@ public class ServerEvents
     @SubscribeEvent
     public static void registerCapabilities(RegisterCapabilitiesEvent event)
     {
-        event.register(Focus.class);
+        event.register(IFocus.class);
+        event.register(SpellManager.class);
+        event.register(UnlockState.class);
     }
 
     @SubscribeEvent
@@ -69,24 +82,26 @@ public class ServerEvents
 
         p.getCapability(FocusProvider.FOCUS).ifPresent(f ->
         {
-            f.updateFocus();
+            f.update();
+            f.sync(p);
 
-            MentalismMessages.sendToPlayer(new FocusValueSyncS2CPacket(f.getFocusPower()), p);
+            // see number of modifiers
+            p.sendMessage(new TextComponent("Number of modifiers: " + f.getModifiers().size() ), p.getUUID());
 
-            /*if(f.getFocusPower() > 1.1f)
+            // see sound distraction amount
+            if(f.hasModifier(Distraction.SOUND_DISTRACTION))
+                p.sendMessage(new TextComponent("Sound distraction amount: " + f.getModifier(Distraction.SOUND_DISTRACTION, Distraction.class).getAmount() ), p.getUUID());
+
+            // if we go over 1.0 focus power, unlock shootArrow spell
+            if(f.getFocusPower() > 1.0f)
             {
-                MentalismMessages.sendToPlayer(new OpenMeditationS2CPacket(), p);
-            }*/
-
-            //p.sendMessage( new TextComponent("Focusing?: " + f.getFocusing()), Util.NIL_UUID);
-            p.sendMessage( new TextComponent("Focus is " + f.getFocusPower() ), Util.NIL_UUID);
-            //p.sendMessage( new TextComponent("Number of focus modifiers is " + f.getModifiers().size()), Util.NIL_UUID);
-            if(f.hasModifier(AntiDistraction.BELL_ANTIDISTRACTION))
-            {
-                AntiDistraction bellAD = (AntiDistraction) f.getModifier(AntiDistraction.BELL_ANTIDISTRACTION);
-                p.sendMessage(new TextComponent("Bell time: " + bellAD.getDecayTime()), Util.NIL_UUID);
-                p.sendMessage(new TextComponent("Bell amount: " + bellAD.getAmount()), Util.NIL_UUID);
-                p.sendMessage(new TextComponent("Bell decayRate: " + bellAD.getTickDecayRate()), Util.NIL_UUID);
+                p.getCapability(UnlockStateProvider.UNLOCK_STATE).ifPresent(um ->
+                {
+                    if(!um.isUnlocked(SpellRegistry.BIDEN_BLAST.get()))
+                    {
+                        um.unlock(SpellRegistry.BIDEN_BLAST.get());
+                    }
+                });
             }
         });
 
@@ -94,41 +109,60 @@ public class ServerEvents
                 sm.update(p));
     }
 
+
+
+    public static final float soundDistractionThreshold = 0.1f;
     @SubscribeEvent
-    public static void entitySoundPlayed(VanillaGameEvent e)
+    public static void entitySoundPlayed(PlaySoundAtEntityEvent e)
     {
-        if(e.getLevel().isClientSide)
+        // if e.getEntity() isn't a ServerPlayer, return
+        if(!(e.getEntity() instanceof ServerPlayer))
             return;
 
-        if(e.getVanillaEvent() == GameEvent.RING_BELL)
+        ServerPlayer p = (ServerPlayer) e.getEntity();
+
+        p.sendMessage(new TextComponent("Sound played: " + e.getSound().getLocation() ), p.getUUID());
+
+        // if the sound is a villager bell sound, add an AntiDistraction modifier to the player's focus
+        if(e.getSound().getLocation().compareTo(SoundEvents.BELL_BLOCK.getLocation()) == 0)
         {
-            for (ServerPlayer p : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers())
-            {
-                if (e.getEventPosition().closerToCenterThan(p.position(), 32f))
+            // make sure we don't add duplicate modifiers
+            p.getCapability(FocusProvider.FOCUS).ifPresent(f -> {
+                if(!f.hasModifier(AntiDistraction.BELL_ANTIDISTRACTION))
                 {
-                    p.getCapability(FocusProvider.FOCUS).ifPresent(f ->
-                    {
-                        if (!f.hasModifier(AntiDistraction.BELL_ANTIDISTRACTION))
-                        {
-                            f.putModifier(new AntiDistraction(f, 4, 0.2f, 3.5f, AntiDistraction.BELL_ANTIDISTRACTION));
-                        }
-                        else
-                        {
-                            AntiDistraction bellAD = (AntiDistraction) f.getModifier(AntiDistraction.BELL_ANTIDISTRACTION);
-                            bellAD.setDecayTime(bellAD.getDecayTime()-0.5f);
-                        }
-                    });
+                    f.putModifier(new AntiDistraction(f, 0, 0.1f, 10f, AntiDistraction.BELL_ANTIDISTRACTION));
                 }
+                else
+                {
+                    f.getModifier(AntiDistraction.BELL_ANTIDISTRACTION, AntiDistraction.class).setDuration(10f);
+                }
+            });
+        }
+
+        // otherwise, add a Distraction modifier
+        else
+        {
+            if (e.getVolume() > soundDistractionThreshold)
+            {
+                p.getCapability(FocusProvider.FOCUS).ifPresent(f -> {
+                    if(!f.hasModifier(Distraction.SOUND_DISTRACTION))
+                    {
+                        f.putModifier(new Distraction(f, 0, 0.1f, 10f, Distraction.SOUND_DISTRACTION));
+                    }
+                    else
+                    {
+                        f.getModifier(Distraction.SOUND_DISTRACTION, Distraction.class).setDuration(10f);
+                    }
+                });
             }
         }
     }
 
+    // interrupt focus when player right clicks a block
     @SubscribeEvent
     public static void playerRightClickedBlock(PlayerInteractEvent.RightClickBlock e)
     {
         e.getPlayer().getCapability(FocusProvider.FOCUS).ifPresent(f ->
-        {
-            f.setFocusPressed(false);
-        });
+                f.setFocusPressed(false));
     }
 }
