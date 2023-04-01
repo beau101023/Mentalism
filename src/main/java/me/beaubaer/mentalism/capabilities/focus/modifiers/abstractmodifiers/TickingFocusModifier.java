0fe0effb0e53fdbf69d46a93b8f4d1e22253aac8
Beau Baer <bbaer10000@gmail.com>
2023-04-01 14:28:09 -0600

    Updated TickingFocusModifier to reflect changes in API

--- src/main/java/me/beaubaer/mentalism/capabilities/focus/modifiers/abstractmodifiers/TickingFocusModifier.java
+++ src/main/java/me/beaubaer/mentalism/capabilities/focus/modifiers/abstractmodifiers/TickingFocusModifier.java
@@ -1,12 +1,10 @@
 package me.beaubaer.mentalism.capabilities.focus.modifiers.abstractmodifiers;
 
-import me.beaubaer.mentalism.capabilities.focus.Focus;
-
 public abstract class TickingFocusModifier extends FocusModifier
 {
-    public TickingFocusModifier(Focus parent, int priority, String ID)
+    public TickingFocusModifier(short priority, String ID)
     {
-        super(parent, priority, ID);
+        super(priority, ID);
     }
 
     public abstract void update();
