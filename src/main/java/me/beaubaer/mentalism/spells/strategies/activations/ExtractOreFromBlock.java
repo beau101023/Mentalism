6ada56330c66c793f1011393cea982d8635efe7f
Beau Baer <bbaer10000@gmail.com>
2023-04-11 15:47:18 -0600

    Made ExtractOreFromBlock put the ore straight into the player's inventory.

--- src/main/java/me/beaubaer/mentalism/spells/strategies/activations/ExtractOreFromBlock.java
+++ src/main/java/me/beaubaer/mentalism/spells/strategies/activations/ExtractOreFromBlock.java
@@ -3,12 +3,15 @@ package me.beaubaer.mentalism.spells.strategies.activations;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.Tags;
+
+import java.util.List;
 
 public class ExtractOreFromBlock
 {
@@ -22,17 +25,20 @@ public class ExtractOreFromBlock
         ServerLevel level = p.getLevel();
 
         BlockPos blockPos = hit.getBlockPos();
-
         BlockState blockState = level.getBlockState(blockPos);
-
-        // make the block drop its item without being destroyed
         Block b = blockState.getBlock();
-        p.awardStat(Stats.BLOCK_MINED.get(b));
-        Block.dropResources(blockState, level, blockPos, null, p, p.getMainHandItem());
+
+        // get the items the block drops when mined with a diamond pickaxe
+        List<ItemStack> drops = Block.getDrops(blockState, level, blockPos, null, p, Items.DIAMOND_PICKAXE.getDefaultInstance());
+
+        // add the drops to the player's inventory
+        for (ItemStack drop : drops)
+        {
+            p.getInventory().placeItemBackInInventory(drop, true);
+        }
 
         // spawn block broken particles
         level.levelEvent(2001, blockPos, Block.getId(blockState));
-
 
         // if the block is a normal ore, replace with stone
         if (blockState.is(Tags.Blocks.ORES_IN_GROUND_STONE))
