4a10080789501e1003ac6bb35a0855344cc8a9f4
Beau Baer <bbaer10000@gmail.com>
2023-04-11 16:16:23 -0600

    Indentation and style fixes

--- src/main/java/me/beaubaer/mentalism/networking/MentalismMessages.java
+++ src/main/java/me/beaubaer/mentalism/networking/MentalismMessages.java
@@ -21,87 +21,103 @@ public class MentalismMessages
         return packetID++;
     }
 
-    public static void register() {
-    SimpleChannel net = NetworkRegistry.ChannelBuilder
-            .named(new ResourceLocation(Mentalism.MOD_ID, "messages"))
-            .networkProtocolVersion(() -> "1.0")
-            .clientAcceptedVersions(s -> true)
-            .serverAcceptedVersions(s -> true)
-            .simpleChannel();
+    public static void register()
+    {
+        SimpleChannel net = NetworkRegistry.ChannelBuilder
+                .named(new ResourceLocation(Mentalism.MOD_ID, "messages"))
+                .networkProtocolVersion(() -> "1.0")
+                .clientAcceptedVersions(s -> true)
+                .serverAcceptedVersions(s -> true)
+                .simpleChannel();
 
-    INSTANCE = net;
+        INSTANCE = net;
 
-    net.messageBuilder(FocusKeySyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-            .decoder(FocusKeySyncC2SPacket::new)
-            .encoder(FocusKeySyncC2SPacket::toBytes)
-            .consumer(FocusKeySyncC2SPacket::handle)
-            .add();
+        net.messageBuilder(FocusKeySyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(FocusKeySyncC2SPacket::new)
+                .encoder(FocusKeySyncC2SPacket::toBytes)
+                .consumer(FocusKeySyncC2SPacket::handle)
+                .add();
 
-    net.messageBuilder(SetFocusTimeC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-            .decoder(SetFocusTimeC2SPacket::new)
-            .encoder(SetFocusTimeC2SPacket::toBytes)
-            .consumer(SetFocusTimeC2SPacket::handle)
-            .add();
+        net.messageBuilder(SetFocusTimeC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(SetFocusTimeC2SPacket::new)
+                .encoder(SetFocusTimeC2SPacket::toBytes)
+                .consumer(SetFocusTimeC2SPacket::handle)
+                .add();
 
-    net.messageBuilder(SetCanFocusC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-            .decoder(SetCanFocusC2SPacket::new)
-            .encoder(SetCanFocusC2SPacket::toBytes)
-            .consumer(SetCanFocusC2SPacket::handle)
-            .add();
+        net.messageBuilder(SetCanFocusC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(SetCanFocusC2SPacket::new)
+                .encoder(SetCanFocusC2SPacket::toBytes)
+                .consumer(SetCanFocusC2SPacket::handle)
+                .add();
 
-    net.messageBuilder(FocusValueSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-            .decoder(FocusValueSyncS2CPacket::new)
-            .encoder(FocusValueSyncS2CPacket::toBytes)
-            .consumer(FocusValueSyncS2CPacket::handle)
-            .add();
+        net.messageBuilder(FocusValueSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(FocusValueSyncS2CPacket::new)
+                .encoder(FocusValueSyncS2CPacket::toBytes)
+                .consumer(FocusValueSyncS2CPacket::handle)
+                .add();
 
-    net.messageBuilder(AvailableSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-            .decoder(AvailableSpellsSyncS2CPacket::new)
-            .encoder(AvailableSpellsSyncS2CPacket::toBytes)
-            .consumer(AvailableSpellsSyncS2CPacket::handle)
-            .add();
+        net.messageBuilder(AvailableSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(AvailableSpellsSyncS2CPacket::new)
+                .encoder(AvailableSpellsSyncS2CPacket::toBytes)
+                .consumer(AvailableSpellsSyncS2CPacket::handle)
+                .add();
 
-    net.messageBuilder(CanCastSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-            .decoder(CanCastSpellsSyncS2CPacket::new)
-            .encoder(CanCastSpellsSyncS2CPacket::toBytes)
-            .consumer(CanCastSpellsSyncS2CPacket::handle)
-            .add();
+        net.messageBuilder(CanCastSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(CanCastSpellsSyncS2CPacket::new)
+                .encoder(CanCastSpellsSyncS2CPacket::toBytes)
+                .consumer(CanCastSpellsSyncS2CPacket::handle)
+                .add();
 
-    net.messageBuilder(SpellProgressSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-            .decoder(SpellProgressSyncS2CPacket::new)
-            .encoder(SpellProgressSyncS2CPacket::toBytes)
-            .consumer(SpellProgressSyncS2CPacket::handle)
-            .add();
+        net.messageBuilder(SpellProgressSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(SpellProgressSyncS2CPacket::new)
+                .encoder(SpellProgressSyncS2CPacket::toBytes)
+                .consumer(SpellProgressSyncS2CPacket::handle)
+                .add();
 
-    net.messageBuilder(SelectedSpellSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-            .decoder(SelectedSpellSyncC2SPacket::new)
-            .encoder(SelectedSpellSyncC2SPacket::toBytes)
-            .consumer(SelectedSpellSyncC2SPacket::handle)
-            .add();
+        net.messageBuilder(SelectedSpellSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(SelectedSpellSyncC2SPacket::new)
+                .encoder(SelectedSpellSyncC2SPacket::toBytes)
+                .consumer(SelectedSpellSyncC2SPacket::handle)
+                .add();
 
-    net.messageBuilder(CastingStateSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-            .decoder(CastingStateSyncC2SPacket::new)
-            .encoder(CastingStateSyncC2SPacket::toBytes)
-            .consumer(CastingStateSyncC2SPacket::handle)
-            .add();
+        net.messageBuilder(CastingStateSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(CastingStateSyncC2SPacket::new)
+                .encoder(CastingStateSyncC2SPacket::toBytes)
+                .consumer(CastingStateSyncC2SPacket::handle)
+                .add();
 
-    net.messageBuilder(SoundDistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-            .decoder(SoundDistractionC2SPacket::new)
-            .encoder(SoundDistractionC2SPacket::toBytes)
-            .consumer(SoundDistractionC2SPacket::handle)
-            .add();
+        net.messageBuilder(SoundDistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
+                .decoder(SoundDistractionC2SPacket::new)
+                .encoder(SoundDistractionC2SPacket::toBytes)
+                .consumer(SoundDistractionC2SPacket::handle)
+                .add();
 
-    net.messageBuilder(BellAntidistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-            .decoder(BellAntidistractionC2SPacket::new)
-            .encoder(BellAntidistractionC2SPacket::toBytes)
-            .consumer(BellAntidistractionC2SPacket::handle)
-            .add();
-}
+        net.messageBuilder(BellAntidistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(BellAntidistractionC2SPacket::new)
+                .encoder(BellAntidistractionC2SPacket::toBytes)
+                .consumer(BellAntidistractionC2SPacket::handle)
+                .add();
 
-    public static <MSG> void sendToServer(MSG message) {
+        net.messageBuilder(AddLingeringEffectS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(AddLingeringEffectS2CPacket::new)
+                .encoder(AddLingeringEffectS2CPacket::toBytes)
+                .consumer(AddLingeringEffectS2CPacket::handle)
+                .add();
+
+        net.messageBuilder(RemoveLingeringEffectS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
+                .decoder(RemoveLingeringEffectS2CPacket::new)
+                .encoder(RemoveLingeringEffectS2CPacket::toBytes)
+                .consumer(RemoveLingeringEffectS2CPacket::handle)
+                .add();
+
+    }
+
+    public static <MSG> void sendToServer(MSG message)
+    {
         INSTANCE.sendToServer(message);
     }
-    public static <MSG> void sendToPlayer(MSG message, ServerPlayer player) {
+    public static <MSG> void sendToPlayer(MSG message, ServerPlayer player)
+    {
         INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), message);
     }
 
