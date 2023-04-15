37ecd5db1e7dfcb8103c3bf4449ac7ceabe9a00f
Beau Baer <bbaer10000@gmail.com>
2023-04-15 13:09:46 -0600

    Refactored redundant code into method

--- src/main/java/me/beaubaer/mentalism/networking/MentalismMessages.java
+++ src/main/java/me/beaubaer/mentalism/networking/MentalismMessages.java
@@ -3,12 +3,16 @@ package me.beaubaer.mentalism.networking;
 import me.beaubaer.mentalism.Mentalism;
 import me.beaubaer.mentalism.networking.C2S.*;
 import me.beaubaer.mentalism.networking.S2C.*;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
 import net.minecraftforge.network.simple.SimpleChannel;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Supplier;
 
 public class MentalismMessages
 {
@@ -32,84 +36,19 @@ public class MentalismMessages
 
         INSTANCE = net;
 
-        net.messageBuilder(FocusKeySyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-                .decoder(FocusKeySyncC2SPacket::new)
-                .encoder(FocusKeySyncC2SPacket::toBytes)
-                .consumer(FocusKeySyncC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(SetFocusTimeC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-                .decoder(SetFocusTimeC2SPacket::new)
-                .encoder(SetFocusTimeC2SPacket::toBytes)
-                .consumer(SetFocusTimeC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(SetCanFocusC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-                .decoder(SetCanFocusC2SPacket::new)
-                .encoder(SetCanFocusC2SPacket::toBytes)
-                .consumer(SetCanFocusC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(FocusValueSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(FocusValueSyncS2CPacket::new)
-                .encoder(FocusValueSyncS2CPacket::toBytes)
-                .consumer(FocusValueSyncS2CPacket::handle)
-                .add();
-
-        net.messageBuilder(AvailableSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(AvailableSpellsSyncS2CPacket::new)
-                .encoder(AvailableSpellsSyncS2CPacket::toBytes)
-                .consumer(AvailableSpellsSyncS2CPacket::handle)
-                .add();
-
-        net.messageBuilder(CanCastSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(CanCastSpellsSyncS2CPacket::new)
-                .encoder(CanCastSpellsSyncS2CPacket::toBytes)
-                .consumer(CanCastSpellsSyncS2CPacket::handle)
-                .add();
-
-        net.messageBuilder(SpellProgressSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(SpellProgressSyncS2CPacket::new)
-                .encoder(SpellProgressSyncS2CPacket::toBytes)
-                .consumer(SpellProgressSyncS2CPacket::handle)
-                .add();
-
-        net.messageBuilder(SelectedSpellSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-                .decoder(SelectedSpellSyncC2SPacket::new)
-                .encoder(SelectedSpellSyncC2SPacket::toBytes)
-                .consumer(SelectedSpellSyncC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(CastingStateSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-                .decoder(CastingStateSyncC2SPacket::new)
-                .encoder(CastingStateSyncC2SPacket::toBytes)
-                .consumer(CastingStateSyncC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(SoundDistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER)
-                .decoder(SoundDistractionC2SPacket::new)
-                .encoder(SoundDistractionC2SPacket::toBytes)
-                .consumer(SoundDistractionC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(BellAntidistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(BellAntidistractionC2SPacket::new)
-                .encoder(BellAntidistractionC2SPacket::toBytes)
-                .consumer(BellAntidistractionC2SPacket::handle)
-                .add();
-
-        net.messageBuilder(AddLingeringEffectS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(AddLingeringEffectS2CPacket::new)
-                .encoder(AddLingeringEffectS2CPacket::toBytes)
-                .consumer(AddLingeringEffectS2CPacket::handle)
-                .add();
-
-        net.messageBuilder(RemoveLingeringEffectS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT)
-                .decoder(RemoveLingeringEffectS2CPacket::new)
-                .encoder(RemoveLingeringEffectS2CPacket::toBytes)
-                .consumer(RemoveLingeringEffectS2CPacket::handle)
-                .add();
-
+        buildMessage(FocusKeySyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER);
+        buildMessage(SetFocusTimeC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER);
+        buildMessage(SetCanFocusC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER);
+        buildMessage(FocusValueSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
+        buildMessage(AvailableSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
+        buildMessage(CanCastSpellsSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
+        buildMessage(SpellProgressSyncS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
+        buildMessage(SelectedSpellSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER);
+        buildMessage(CastingStateSyncC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER);
+        buildMessage(SoundDistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_SERVER);
+        buildMessage(BellAntidistractionC2SPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
+        buildMessage(AddLingeringEffectS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
+        buildMessage(RemoveLingeringEffectS2CPacket.class, id(), NetworkDirection.PLAY_TO_CLIENT);
     }
 
     public static <MSG> void sendToServer(MSG message)
@@ -121,16 +60,42 @@ public class MentalismMessages
         INSTANCE.send(PacketDistributor.PLAYER.with(() -> player), message);
     }
 
-    /*private static <MSG> void buildMessage(Class<MSG> clazz, int id, NetworkDirection direction)
+    private static <MSG extends IMessage> void buildMessage(Class<MSG> clazz, int id, NetworkDirection direction)
     {
-        // TODO: Look at this again later
-
-        // build message as in register()
-
         INSTANCE.messageBuilder(clazz, id, direction)
-                .decoder(MSG::new)
-                .encoder(clazz::toBytes)
-                .consumer(clazz::handle)
-                .add();
-    }*/
+                .decoder(buf ->
+                {
+                    try
+                    {
+                        return clazz.getConstructor(FriendlyByteBuf.class).newInstance(buf);
+                    }
+                    catch (InstantiationException | NoSuchMethodException | InvocationTargetException |
+                           IllegalAccessException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .encoder((msg, buf) ->
+                {
+                    try
+                    {
+                        clazz.getMethod("toBytes", FriendlyByteBuf.class).invoke(msg, buf);
+                    }
+                    catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .consumer((msg, ctx) ->
+                {
+                    try
+                    {
+                        clazz.getMethod("handle", Supplier.class).invoke(msg, ctx);
+                    }
+                    catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
+                    {
+                        throw new RuntimeException(e);
+                    }
+                }).add();
+    }
 }
